77b07411e6899dcc571fc2fd59168277f821a420
inno <893930581@qq.com>
2020-09-13 11:45:00 +0800

    feat(leetcode): 105, 106, 107, 111, 174

--- src/playground/leetcode/104maximum-depth-of-binary-tree/maxDepth.ts
+++ src/playground/leetcode/104maximum-depth-of-binary-tree/maxDepth.ts
@@ -1,4 +1,4 @@
-import TreeNode from "../../../utils/leetcode/TreeNode";
+// import TreeNode from "../../../utils/leetcode/TreeNode.ts";
 /**
  * Definition for a binary tree node.
  * class TreeNode {
@@ -18,3 +18,38 @@ export default function maxDepth(root: TreeNode | null): number {
         ? 0
         : Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
 }
+
+// function breakfastNumber(
+//     staple: number[],
+//     drinks: number[],
+//     x: number
+// ): number {
+//     let m = staple.length;
+//     let n = drinks.length;
+//     let count = 0;
+//     staple.sort((a, b) => {
+//         return a - b;
+//     });
+//     drinks.sort((a, b) => {
+//         return a - b;
+//     });
+//     console.log(staple, drinks);
+//     for (let i = 0; i < m; i++) {
+//         if (staple[i] >= x) {
+//             break;
+//         }
+//         for (let j = 0; j < n; j++) {
+//             console.log(staple[i], drinks[j]);
+//             if (staple[i] + drinks[j] > x) {
+//                 break;
+//             }
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// let staple = [10, 20, 5],
+//     drinks = [5, 5, 2],
+//     x = 15;
+// let res = breakfastNumber(staple, drinks, x);
+// console.log(res);
--- src/playground/leetcode/174dungeon-game/calculateMinimumHP.ts
+++ src/playground/leetcode/174dungeon-game/calculateMinimumHP.ts
@@ -1,34 +1,28 @@
-// [initalHP, currHP];
-function calculateMinimumHP(dungeon: number[][]): number {
-    // TODO
+export default function calculateMinimumHP(dungeon: number[][]): number {
     let n = dungeon.length;
-    let dp: number[][][] = new Array(n);
     if (!n) return 0;
     let m = dungeon[0].length;
-    for (let i = 0; i < n; i++) {
-        dp[i] = new Array(m).fill([0, 0]);
+
+    let dp: number[][] = new Array(n);
+    for (let i = 0; i < n + 1; i++) {
+        dp[i] = new Array(m + 1).fill(Number.MAX_VALUE);
     }
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            let currHPCost = dungeon[i][j] > 0 ? 0 : -dungeon[i][j];
-            let currHP = currHPCost === 0 ? dungeon[i][j] : 0;
-            if (i === 0 && j === 0) {
-                dp[i][j] = [currHPCost, currHP];
-            } else if (i === 0) {
-                dp[i][j] = [
-                    dp[i][j - 1][0] + currHPCost,
-                    dp[i][j - 1][1] + currHP,
-                ];
-            } else if (i === 0) {
-                dp[i][j] = [
-                    dp[i - 1][j][0] + currHPCost,
-                    dp[i - 1][j][1] + currHP,
-                ];
-            } else {
-                dp[i][j] = [
-                    Math.min(dp[i - 1][j][0], dp[i][j - 1][0]) + currHPCost,
-                ];
-            }
+    dp[n - 1][m] = dp[n][m - 1] = 1;
+
+    for (let i = n - 1; i >= 0; i--) {
+        for (let j = m - 1; j >= 0; j--) {
+            let bestChoice = Math.min(dp[i + 1][j], dp[i][j + 1]);
+            dp[i][j] = bestChoice - dungeon[i][j];
         }
     }
+    return dp[0][0] <= 0 ? 0 : dp[0][0];
 }
+
+calculateMinimumHP([[0, 0]]);
+
+/* 
+ -2 (K)	-3	     3      
+ -5	    -10	     1      1
+ 10	     30	    -5 (P)  
+                 1
+*/
